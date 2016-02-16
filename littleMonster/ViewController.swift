b80befa13037161197271b99f7d1be458b68808b
laoman <indefixx@gmail.com>
2016-02-16 19:23:28 +0200

    Initial Commit

---
+++ littleMonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  littleMonster
+//
+//  Created by Georgios Georgiou on 16/02/16.
+//  Copyright © 2016 gig.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
